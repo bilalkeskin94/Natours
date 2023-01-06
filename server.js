1d1373cdbe8aa3ee6ad79d78ad21fcd7c4f8842e
bilalkeskinosf <105927173+bilalkeskinosf@users.noreply.github.com>
2023-01-07 00:37:08 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is started on port ${port}`);
+});
